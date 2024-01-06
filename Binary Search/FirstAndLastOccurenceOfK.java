97456688709ceccfdea9d7e77cf863315e1164b8
ahan-stark <ahanabubakar@gmail.com>
2024-01-07 00:51:10 +0530

    initial commit

--- Binary Search/FirstAndLastOccurenceOfK.java
+++ Binary Search/FirstAndLastOccurenceOfK.java
@@ -9,7 +9,10 @@ public class FirstAndLastOccurenceOfK {
         System.out.println("Brute Force = " + " first : " + bruteForce[0] + " second : " + bruteForce[1]);
         // better approach using lower bound and upper bound
         int betterApproach[] = solution.betterApproach(arr, elem);
-        System.out.println("Brute Force = " + " first : " + betterApproach[0] + " second : " + betterApproach[1]);
+        System.out.println("Better  Approach = " + " first : " + betterApproach[0] + " second : " + betterApproach[1]);
+        // optimal approach
+        int optimalApproach[] = solution.optimalApproach(arr, elem);
+        System.out.println("Optimal Approach = " + "first : " + optimalApproach[0] + " second : " + optimalApproach[1]);
     }
 
     private static class Solution {
@@ -36,6 +39,39 @@ public class FirstAndLastOccurenceOfK {
             // hence the upper bound points to the next element add - 1;
             int upperB = upperBound(arr, elem);
             return new int[] { lowB, upperB - 1 };
+        }
+
+        private int[] optimalApproach(int arr[], int elem) {
+            int lowIndex = arr.length;
+            int highIndex = arr.length;
+            int low = 0, high = arr.length - 1;
+            // get the lowIndex by finding the element and going extreme left
+            while (low <= high) {
+                int mid = (low + high) / 2;
+                if (arr[mid] == elem) {
+                    lowIndex = mid;
+                    high = mid - 1;
+                } else if (arr[mid] < elem) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+            // get the highIndex by finding the element and going extreme right
+            low = 0;
+            high = arr.length - 1;
+            while (low <= high) {
+                int mid = (low + high) / 2;
+                if (arr[mid] == elem) {
+                    highIndex = mid;
+                    low = mid + 1;
+                } else if (arr[mid] < elem) {
+                    low = mid + 1;
+                } else {
+                    high = mid - 1;
+                }
+            }
+            return new int[] { lowIndex, highIndex };
         }
     }
 
