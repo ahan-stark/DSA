28087e6e9b0cd4a726e70c8d52b89b1cda49a7c5
ahan-stark <ahanabubakar@gmail.com>
2024-01-15 21:12:51 +0530

    initial commit

--- Binary Search/KthMissingPositiveNumber.java
+++ Binary Search/KthMissingPositiveNumber.java
@@ -5,15 +5,15 @@ public class KthMissingPositiveNumber {
         Solution solution = new Solution();
         int bruteForce = solution.bruteForce(arr, k);
         System.out.println("Brute Force : " + bruteForce);
-
+        int binarySearch = solution.binarySearch(arr, k);
+        System.out.println("Binary Search : " + binarySearch);
     }
 
     private static class Solution {
-
-        public int bruteForce(int[] arr, int k) {
+        private int bruteForce(int[] arr, int k) {
             // for an empty array the mising 3 value is 3 , so for array containg sorted
             // values if the elem is less than k increase the k value until k is less than
-            // elem
+            // elem and that elem is the missing one
             for (int ele : arr) {
                 if (ele <= k) {
                     k++;
@@ -23,6 +23,19 @@ public class KthMissingPositiveNumber {
             }
             return k;
         }
-
+        private int binarySearch(int arr[], int k) {
+            int low = 0, high = arr.length - 1;
+            while (low <= high) {
+                int mid = (low + high) / 2;
+                // considering 1 should conatin 1 , 2->2,3->3...in same we will check if in
+                // 5-> 8 then 8 - 5 = 3 is the missing numbers
+                int missNumbers = arr[mid] - (mid + 1);
+                if (missNumbers < k)
+                    low = mid + 1;
+                else
+                    high = mid - 1;
+            }
+            return low + k;
+        }
     }
 }
