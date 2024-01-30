45e955ff0bab3e4beee71259e41685e0abbc1380
ahan-stark <ahanabubakar@gmail.com>
2024-01-31 01:08:03 +0530

    initial commit

---
+++ Binary Search/RowWithMaximumNumberOf1.java
@@ -0,0 +1,56 @@
+public class RowWithMaximumNumberOf1 {
+    public static void main(String[] args) {
+        int arr[][] = { { 0, 1, 1 }, { 1, 1, 1 }, { 0, 0, 1 }, { 0, 0, 0 } };
+        Solution solution = new Solution();
+        int bruteForce = solution.bruteForce(arr);
+        System.out.println("Brute Force : " + bruteForce);
+        int binarySearch = solution.binarySearch(arr);
+        System.out.println("Binary Search : " + binarySearch);
+    }
+
+    private static class Solution {
+        private int bruteForce(int arr[][]) {
+            int current = 0;
+            int index = -1;
+            for (int i = 0; i < arr.length; i++) {
+                int count = 0;
+                for (int j = 0; j < arr[0].length; j++) {
+                    if (arr[i][j] == 1)
+                        count++;
+                }
+                if (count > current) {
+                    current = count;
+                    index = i;
+                }
+            }
+            return index;
+        }
+
+        private int binarySearch(int arr[][]) {
+            int index = -1;
+            int ans = 0;
+            for (int i = 0; i < arr.length; i++) {
+                int sol = checkRows(arr[i]);
+                if ((arr.length - sol) > ans) {
+                    ans = (arr.length - sol);
+                    index = i;
+                }
+            }
+            return index;
+        }
+
+        private int checkRows(int[] row) {
+            int low = 0;
+            int high = row.length - 1;
+            while (low <= high) {
+                int mid = (low + high) / 2;
+                if (row[mid] >= 1) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
+            }
+            return low;
+        }
+    }
+}
