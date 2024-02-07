0097fdc581ab0109fb2ddadca46f6e0756a49b4e
ahan-stark <ahanabubakar@gmail.com>
2024-02-07 11:33:01 +0530

    initial commit

--- Linked List/ReverseLL.java
+++ Linked List/ReverseLL.java
@@ -2,7 +2,7 @@ import java.util.Stack;
 
 public class ReverseLL {
     public static void main(String[] args) {
-        int arr[] = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        int arr[] = { 1, 2, 3, 4, 5};
         Node head = new Node(arr[0]);
         Node cur = head;
         for (int i = 1; i < arr.length; i++) {
