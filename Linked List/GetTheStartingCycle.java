7a0edabc0c0fa38f9ba96ac9e708de4a82a43bdb
ahan-stark <ahanabubakar@gmail.com>
2024-02-08 00:53:06 +0530

    initial commit

--- Linked List/GetTheStartingCycle.java
+++ Linked List/GetTheStartingCycle.java
@@ -1,6 +1,6 @@
 import java.util.HashMap;
 
-public class DetectLoopInLL {
+public class GetTheStartingCycle {
     public static void main(String[] args) {
         Solution solution = new Solution();
         Node head = new Node(1);
@@ -15,36 +15,45 @@ public class DetectLoopInLL {
         fourth.next = fifth;
         // Create a loop
         fifth.next = third;
-        boolean bruteForce = solution.bruteForce(head);
-        System.out.println("Cycle Exists in Linked List = " + bruteForce);
-        boolean optimalSol = solution.optimalSolution(head);
-        System.out.println("Cycle Exists in Linked List = " + optimalSol);
+        Node bruteForce = solution.bruteForce(head);
+        System.out.println("Starting Node of Cycle : " + bruteForce.data);
+        Node optimalSol = solution.optimalSol(head);
+        System.out.println("Starting Node of Cycle : " + optimalSol.data);
+
     }
 
     private static class Solution {
-        private boolean bruteForce(Node head) {
+        private Node bruteForce(Node head) {
             HashMap<Node, Integer> map = new HashMap<>();
             while (head != null) {
                 if (map.containsKey(head))
-                    return true;
+                    return head;
                 else {
                     map.put(head, 1);
                     head = head.next;
                 }
             }
-            return false;
+            return null;
         }
 
-        private boolean optimalSolution(Node head) {
+        private Node optimalSol(Node head) {
             Node fast = head;
             Node slow = head;
             while (fast != null && fast.next != null) {
                 slow = slow.next;
                 fast = fast.next.next;
-                if (slow == fast)
-                    return true;
+                // if they collide take the slow from start and fast from collision and move
+                // again untill they meet each other, the point of meeting is starting of cycle
+                if (fast == slow) {
+                    slow = head;
+                    while (slow != fast) {
+                        slow = slow.next;
+                        fast = fast.next;
+                    }
+                    return slow;
+                }
             }
-            return false;
+            return null;
         }
     }
 
