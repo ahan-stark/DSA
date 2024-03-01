982340b8bab73425a1e4f15d026d17f6168b811c
ahan-stark <ahanabubakar@gmail.com>
2024-03-01 16:17:25 +0530

    initial commit

---
+++ String/ValidAnagram.java
@@ -0,0 +1,28 @@
+import java.util.HashMap;
+
+public class ValidAnagram {
+    public static void main(String[] args) {
+        String str1 = "aacc", str2 = "ccaa";
+        Solution solution = new Solution();
+        boolean betterApproach = solution.betterApproach(str1, str2);
+        System.out.println("Are the strings Anagram? : " + betterApproach);
+
+    }
+    private static class Solution {
+        private boolean betterApproach(String str1, String str2) {
+            int arr[] = new int[26];
+            for (int i = 0; i < str1.length(); i++) {
+                arr[str1.charAt(i) - 97]++;
+            }
+            for (int i = 0; i < str2.length(); i++) {
+                arr[str2.charAt(i) - 97]--;
+            }
+            for (int i = 0; i < 26; i++) {
+                if (arr[i] != 0) {
+                    return false;
+                }
+            }
+            return true;
+        }
+    }
+}
