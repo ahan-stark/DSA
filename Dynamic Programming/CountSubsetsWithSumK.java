8fc7aeb48671ede644d3ae1a0972cdb8fd967616
ahan-stark <ahanabubakar@gmail.com>
2025-12-26 20:49:43 +0530

    initial commit

--- Dynamic Programming/CountSubsetsWithSumK.java
+++ Dynamic Programming/CountSubsetsWithSumK.java
@@ -12,13 +12,14 @@ public class CountSubsetsWithSumK {
     static int dp[][];
 
     public static void main(String[] args) {
-        int arr[] = {2, 3, 5, 16, 8, 10};
+        int arr[] = { 2, 3, 5, 16, 8, 10 };
         int k = 10;
-         dp = new int[arr.length][k + 1];
-        for(int[] temp : dp){
+        dp = new int[arr.length][k + 1];
+        for (int[] temp : dp) {
             Arrays.fill(temp, -1);
         }
-        System.out.println("Total subsets with sum = " + k + " are : " + recursion(arr, k , 0)); 
+        System.out.println("Total subsets with sum = " + k + " are : " + recursion(arr, k, 0));
+        System.out.println("Total subsets with sum = " + k + " are : " + tabulation(arr, k));
     }
 
     private static int recursion(int[] arr, int k, int curIndex) {
@@ -34,5 +35,28 @@ public class CountSubsetsWithSumK {
         int noPick = recursion(arr, k, curIndex + 1);
         dp[curIndex][k] = (int) ((pick + noPick) % (1e9 + 7));
         return dp[curIndex][k];
+    }
+
+    private static int tabulation(int[] arr, int K) {
+        int MODULO = 10000000;
+        int n = arr.length;
+        int[][] dp = new int[n][K + 1];
+        for (int i = 0; i < n; i++) {
+            dp[i][0] = 1;
+        }
+        if (arr[0] <= K) {
+            dp[0][arr[0]] = 1;
+        }
+        for (int ind = 1; ind < n; ind++) {
+            for (int target = 1; target <= K; target++) {
+                int notTaken = dp[ind - 1][target];
+                int taken = 0;
+                if (arr[ind] <= target) {
+                    taken = dp[ind - 1][target - arr[ind]] % MODULO;
+                }
+                dp[ind][target] = (notTaken + taken) % MODULO;
+            }
+        }
+        return dp[n - 1][K];
     }
 }
