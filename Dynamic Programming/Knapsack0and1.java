6125a92139d57cff987f642e939eae8008f50ff4
ahan-stark <ahanabubakar@gmail.com>
2026-01-05 23:36:09 +0530

    initial commit

--- Dynamic Programming/Knapsack0and1.java
+++ Dynamic Programming/Knapsack0and1.java
@@ -26,6 +26,7 @@ public class Knapsack0and1 {
             Arrays.fill(temp, -1);
         }
         System.out.println("Max vals that can be obtained is " + recursion(0, wt, val, totalWeight));
+        System.out.println("Max vals that can be obtained is " + tabulation(wt, val, totalWeight));
 
     }
 
@@ -42,6 +43,32 @@ public class Knapsack0and1 {
         dp[index][totalWeight] = Math.max(pick, noPick);
         return dp[index][totalWeight];
     }
+
+    private static int tabulation(int[] wt, int[] val, int W) {
+        int dp[][] = new int[wt.length][W + 1];
+        for (int i = wt[0]; i <= W; i++) {
+            dp[0][i] = val[0];
+        }
+        for (int i = 1; i < val.length; i++) {
+            for (int weight = 1; weight <= W; weight++) {
+                int noPick = dp[i - 1][weight];
+                int pick = 0;
+                if (wt[i] <= weight) {
+                    pick = val[i] + dp[i - 1][weight - wt[i]];
+                }
+                dp[i][weight] = Math.max(pick, noPick);
+            }
+        }
+        return dp[dp.length - 1][dp[0].length - 1];
+    }
 }
 // same pick and non pick algo
 // before you pick check once can i take that weight, if yes pick that
+
+// tabulation
+// We start assigning the initial dp with values of "wt" that can be choosen,
+// for wt[0] which ever is greater than or equal to that, we put the first
+// value, meaning when we are in index 2 and 'wt' we try to access, the weight
+// should be greater than or equal to allowed weight for first one
+// if first wt is 4, only the wt 4 or above can access the value from that
+// index, else it cannot access, meaning the value will be 0
