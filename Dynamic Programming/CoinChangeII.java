afd5b4317e0c2e15e7eeaa298051fc1db4f04863
ahan-stark <ahanabubakar@gmail.com>
2026-01-16 00:03:03 +0530

    initial commit

--- Dynamic Programming/CoinChangeII.java
+++ Dynamic Programming/CoinChangeII.java
@@ -32,6 +32,8 @@ public class CoinChangeII {
         for (int[] arr : dp)
             Arrays.fill(arr, -1);
         System.out.println("Total combinations : " + recursion(0, coins, amount));
+        System.out.println("Total combinations : " + tabulation(coins, amount));
+
     }
 
     private static int recursion(int index, int[] coins, int amount) {
@@ -47,6 +49,30 @@ public class CoinChangeII {
         int noPick = recursion(index + 1, coins, amount);
         dp[index][amount] = (pickVal + noPick) % ((int) 1e9 + 7);
         return dp[index][amount];
+    }
+
+    public static int tabulation(int[] coins, int amount) {
+        int mod = (int) 1e9 + 7;
+        int dp[][] = new int[coins.length][amount + 1];
+        for (int i = 0; i < coins.length; i++) {
+            dp[i][0] = 1;
+        }
+        for (int amt = 0; amt <= amount; amt++) {
+            if (amt % coins[0] == 0) {
+                dp[0][amt] = 1;
+            }
+        }
+        for (int i = 1; i < coins.length; i++) {
+            for (int amt = 0; amt <= amount; amt++) {
+                int noTake = dp[i - 1][amt];
+                int take = 0;
+                if (coins[i] <= amt) {
+                    take = dp[i][amt - coins[i]];
+                }
+                dp[i][amt] = (take + noTake) % mod;
+            }
+        }
+        return dp[dp.length - 1][amount];
     }
 }
 // one thing we observe is all values will be unique, if we start from the start
