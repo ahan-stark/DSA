b52a703dec30b87bd14dfb0e046481b40fc43896
ahan-stark <ahanabubakar@gmail.com>
2026-01-27 02:31:09 +0530

    initial commit

--- Dynamic Programming/RodCuttingProblem.java
+++ Dynamic Programming/RodCuttingProblem.java
@@ -16,6 +16,8 @@ public class RodCuttingProblem {
             Arrays.fill(arr, -1);
         System.out.println("Maximum prices that can be obtained by cutting rods in : " + n + " index value are : "
                 + recursion(0, price, n));
+        System.out.println("Maximum prices that can be obtained by cutting rods in : " + n + " index value are : "
+                + tabulation(price, n));
     }
 
     private static int recursion(int index, int[] price, int target) {
@@ -31,7 +33,28 @@ public class RodCuttingProblem {
         dp[index][target] = Math.max(pick, noPick);
         return dp[index][target];
     }
+
+    public static int tabulation(int price[], int n) {
+        int dp[][] = new int[price.length + 1][n + 1];
+        for (int i = 1; i <= n; i++) {
+            dp[1][i] = price[0] * i;
+        }
+        for (int len = 2; len <= price.length; len++) {
+            for (int target = 1; target <= n; target++) {
+                int noPick = dp[len - 1][target];
+                int pick = 0;
+                if (target >= len) {
+                    pick = price[len - 1] + dp[len][target - len];
+                }
+                dp[len][target] = Math.max(pick, noPick);
+            }
+        }
+        return dp[dp.length - 1][n];
+    }
 }
 // same pick and no pick algo
 // since we need to consider index from 1-> n whenever we choose index make sure
 // we decrement the target as target - (index + 1) not target - index
+
+// tabulation, since the question is for 1 based indexing, so add base case for
+// index 1, then extend dp upto <=n
